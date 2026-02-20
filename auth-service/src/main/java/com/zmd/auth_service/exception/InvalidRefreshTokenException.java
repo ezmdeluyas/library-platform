840a6a7a27eeb0c9efc7f2924100ddba2f88cff4
ezmdeluyas <ezdeluyas@gmail.com>
2026-02-20 17:34:18 +0800

    chore: removed the RefreshToken from response

--- auth-service/src/main/java/com/zmd/auth_service/exception/InvalidRefreshTokenException.java
+++ auth-service/src/main/java/com/zmd/auth_service/exception/InvalidRefreshTokenException.java
@@ -1,7 +1,7 @@
 package com.zmd.auth_service.exception;
 
 public class InvalidRefreshTokenException extends RuntimeException {
-    public InvalidRefreshTokenException(String refreshToken) {
-        super("Invalid refresh token: " + refreshToken);
+    public InvalidRefreshTokenException() {
+        super("Invalid refresh token");
     }
 }
