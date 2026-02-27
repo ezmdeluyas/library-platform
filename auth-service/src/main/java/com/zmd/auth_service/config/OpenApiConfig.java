fbfa0682dbc7aa8c25ea2508a96a8341f3b76844
ezmdeluyas <ezdeluyas@gmail.com>
2026-02-27 19:02:08 +0800

    chore: removed line that globally added auth for every request in the api docs when imported to software like postman

--- auth-service/src/main/java/com/zmd/auth_service/config/OpenApiConfig.java
+++ auth-service/src/main/java/com/zmd/auth_service/config/OpenApiConfig.java
@@ -15,7 +15,6 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
