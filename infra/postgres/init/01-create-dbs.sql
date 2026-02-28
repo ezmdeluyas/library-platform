68ee4b55583496d1c41befed4c6f013d39f2e480
ezmdeluyas <ezdeluyas@gmail.com>
2026-02-28 21:19:32 +0800

    chore: updated infra create db script

--- infra/postgres/init/01-create-dbs.sql
+++ infra/postgres/init/01-create-dbs.sql
@@ -1,2 +1,6 @@
-﻿CREATE DATABASE authdb;
-CREATE DATABASE librarydb;
+﻿-- 01-create-dbs.sql
+SELECT 'CREATE DATABASE authdb'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'authdb')\gexec;
+
+SELECT 'CREATE DATABASE librarydb'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'librarydb')\gexec;
