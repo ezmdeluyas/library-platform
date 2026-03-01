f1293f9ae4747a9acb5992a3ad56d50bb9ea8799
ezmdeluyas <ezdeluyas@gmail.com>
2026-03-01 14:00:33 +0800

    chore: converted enum values and check constraint to uppercase to match with java convention thus no more converters

--- library-service/src/main/resources/db/migration/V1__library_types.sql
+++ library-service/src/main/resources/db/migration/V1__library_types.sql
@@ -4,6 +4,6 @@
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'copy_status_enum') THEN
-CREATE TYPE copy_status_enum AS ENUM ('available', 'borrowed', 'lost', 'damaged');
+CREATE TYPE copy_status_enum AS ENUM ('AVAILABLE', 'BORROWED', 'LOST', 'DAMAGED');
 END IF;
 END$$;
