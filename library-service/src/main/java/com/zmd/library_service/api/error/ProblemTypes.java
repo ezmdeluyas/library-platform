36d8187339e753b35b3754655fcdf4b8052d82c3
ezmdeluyas <ezdeluyas@gmail.com>
2026-03-14 11:17:33 +0800

    chore: added api problem detail constants

---
+++ library-service/src/main/java/com/zmd/library_service/api/error/ProblemTypes.java
@@ -0,0 +1,12 @@
+package com.zmd.library_service.api.error;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ProblemTypes {
+    public static final String BUSINESS_RULE_VIOLATION = "business-rule-violation";
+    public static final String RESOURCE_NOT_FOUND = "resource-not-found";
+    public static final String INTERNAL_ERROR = "internal-error";
+    public static final String VALIDATION_ERROR = "validation-error";
+}
